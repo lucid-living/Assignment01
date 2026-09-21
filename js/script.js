cca88b780a9d94423f20d11308c6f9f93eacc7de
lucid-living <simply.lucid.living@gmail.com>
2026-09-21 02:23:53 -0700

    final edits

--- js/script.js
+++ js/script.js
@@ -29,8 +29,121 @@
     let blnContApproval
     */
 //STEP 5
-let strLastName = 'Vallejo'
-let strCity
-strCity = 'San Diego'
+    /*
+	let strLastName = 'Vallejo'
+	let strCity
+	strCity = 'San Diego'
 
-let strFirstName, strLastName, strCity = 'Jose','Vallejo', 'San Diego'
+	let strFirstName = 'Jose', 
+        strLastName = 'Vallejo',
+        strCity = 'San Diego'
+	*/
+//STEP 6
+	/*	
+	let statement = 'I am ' + 27
+	window.console.log(statement) */
+//STEP 7
+	/*
+	let bar = false + ' idol'
+	console.log(bar)
+	let addingWords = 10 + true
+	console.log(addingWords)
+	*/
+//STEP 8
+	/*
+	 it is not valid, there is an apostrophe that gets caught in "I'm not sure..." so what ever comes after doesn't print
+
+	 we could use a backslash so we treat the apostrophe as a string and not a literal. We will end up like this:
+
+	let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+	console.log(someString)
+	 */
+//STEP 9
+	/*
+	 let nullValue = null
+	 console.log(nullValue)
+
+	 let undValue
+	 console.log(undValue)
+	 */
+
+//STEP 10
+	/*
+ 	console.log(typeof 'string')
+	console.log(typeof 3)
+	console.log(typeof true)
+	console.log(typeof null)
+	console.log(typeof sup)
+	
+	*/
+//STEP 11
+	/*
+	 alert('Hello ' + 'Jose Vallejo' + ', welcome to the JavaScript class!')
+	 */
+//STEP 12 
+/*
+ let name = 'Jose'
+	 alert('Hello ' + name + ', welcome to the JavaScript class!')
+*/
+
+//STEP 13 
+/*
+ let course = 'JavaScript'
+ let name = 'Jose'
+
+	 alert('Hello ' + name + ', welcome to the ' + course + ' class!')
+	*/
+//STEP 14 
+/*
+ 
+	 alert('Hello ' + name + ', \nWelcome to the ' + course + ' class!')
+	*/
+//STEP 15
+/*
+ let name = prompt('Hello what\'s your name?')
+	*/
+//STEP 16
+/*
+	let course = prompt('what course are you taking?')
+ 	*/
+//STEP 17
+/*
+ 	let x = 10
+	let y = 20
+	console.log(x + y)
+
+	*/
+//STEP 18
+/*
+	let x
+	x= 20
+
+	x+=20
+
+	console.log(x)
+ 	*/
+//STEP 19
+/*
+ let x = 20
+ x*=5
+
+ console.log(x)
+	*/
+
+//STEP 20
+/*
+ let x = 20%3
+ x /= 1
+ console.log(x)
+	*/
+//STEP 21 
+/*
+ let x = 3>1 || 4==4
+ console.log(x)
+ */
+
+//STEP 22
+/*
+ let i = 7>9 && 57<59
+ console.log(i)
+  */

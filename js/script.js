8386f56df0ecd36a2edc68d997aaae85a688c13a
lucid-living <simply.lucid.living@gmail.com>
2026-09-20 23:58:49 -0700

    Completed JavaScript assignment 01 exercises

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//STEP 1
+    /*let someMonth
+    function theMonth()
+    let currentMonth
+    let summerMonth
+    let myLibraryFunction */
+
+//STEP 2
+    /*3
+    'dog'
+    true
+    null*/
+
+//STEP 3
+    /* 
+    let monthAndDay = 'January the ' + 3
+    let height = 3 + ' feet'
+    */
+//STEP 4
+    /*
+    let strFirstName
+    let strLastName
+    let strAddress
+    let strCity
+    let strState
+    let intZipCode
+    let intAge
+    let strRefSource
+    let blnContApproval
+    */
+//STEP 5
+let strLastName = 'Vallejo'
+let strCity
+strCity = 'San Diego'
+
+let strFirstName, strLastName, strCity = 'Jose','Vallejo', 'San Diego'
